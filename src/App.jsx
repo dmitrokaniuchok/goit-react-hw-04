66da3bb545d1446f40a05b388784fa68e4376b00
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-05-10 19:00:03 +0300

    add steps

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,33 @@ import { fetchImages } from "./components/api";
 import toast from "react-hot-toast";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
 
 export default function App() {
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState("");
   const [inputValue, setInputValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     if (!query) return;
 
-    fetchImages(query).then((data) => {
-      setImages(data.results);
-    });
-  }, [query]);
+    setIsLoading(true);
+    fetchImages(query, page)
+      .then((data) => {
+        if (data.results.length === 0 && page === 1) {
+          toast.error("No images found.");
+        }
+        setImages((prev) => [...prev, ...data.results]);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        toast.error("Failed to load images.");
+      });
+  }, [query, page]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -24,17 +38,27 @@ export default function App() {
       return;
     }
     setQuery(inputValue);
+    setImages([]);
+    setPage(1);
     setInputValue("");
+  };
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
   };
 
   return (
     <div>
       <SearchBar
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        onChange={(event) => setInputValue(event.target.value)}
         onSubmit={handleSubmit}
       />
       {images.length > 0 && <ImageGallery images={images} />}
+      {isLoading && <Loader isOverlay={false} />}
+      {images.length > 0 && !isLoading && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
     </div>
   );
 }
---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,11 @@
+import styles from "./LoadMoreBtn.module.css";
+
+export default function LoadMoreBtn({ onClick }) {
+  return (
+    <>
+      <button type="button" className={styles.button} onClick={onClick}>
+        Load more
+      </button>
+    </>
+  );
+}
